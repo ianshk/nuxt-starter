e0f13f4c3171086505a2e9345f58644b2689e9ff
Ian Callaghan <ianshk@gmail.com>
2022-08-18 11:15:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Basic Nuxt 3 Starter
+
+- Nuxt 3.0 RC
+- Tailwind CSS v3 via Nuxt Tailwind https://tailwindcss.nuxtjs.org/
+- Prettier
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineProps<{
+  greeting: string;
+}>();
+</script>
+
+<template>
+  <div class="text-3xl font-bold underline">{{ greeting }}</div>
+</template>
